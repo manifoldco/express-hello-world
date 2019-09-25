a0c0c5ad9af71745f8fa24009ecbf960647788af
Drew Powers <drew@pow.rs>
2019-09-25 10:55:40 -0600

    Remove LogDNA

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
-const LogDNA = require('logdna');
-const log = LogDNA.setupDefaultLogger(process.env.KEY, {});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,7 +12,7 @@ app.use(express.static('public'));
 app.get('/', (_, res) => res.sendFile(__dirname + '/views/index.html'));
 app.post('/api/log', (req, res) => {
   try {
-    log.log(req.body.log);
+    console.log(req.body.log);
     res.send(`Logged: ${req.body.log}`);
   } catch (e) {
     res.send(e);
